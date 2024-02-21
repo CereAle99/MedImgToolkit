ef9859401713f504dbeede79cd738fd7da3eac4c
Ale <alessandro@ceresi.eu>
2024-02-21 01:15:12 +0100

    added documentation to the function

--- lib/binarize.py
+++ lib/binarize.py
@@ -1,19 +1,34 @@
 import nibabel as nib
 
 
-def binarize(input_nifti, label):
+def binarize(input_image, label):
+    """
+    Binarize a nibabel (nib) images. All the grey values in the image which level
+    is equal to the label are put to 1, all the others are put to 0. 
+    
+    Args:
+        input_image: nib
+            imput object of the image to be binarized
+        label: float or int
+            label of the voxel that has to be put to 1
 
-    # Load the nifti file image
-    image = input_nifti.get_fdata()
+    Returns: 
+        aligned_input: nib
+            input_image binarized
+
+    """
+    
+    # Load the nibabel object image
+    array = input_image.get_fdata()
 
     # Binarize the image for the label value
     if label == 1:
         pass
     elif label:
-        image[image != label] = 0
-        image[image == label] = 1
+        array[array != label] = 0
+        array[array == label] = 1
 
-    # Save the image in nifti file
-    binarized_nifti = nib.Nifti1Image(image, input_nifti.affine, input_nifti.header)
+    # Save the image in nibabel object
+    binarized_image = nib.Nifti1Image(array, input_image.affine, input_image.header)
 
-    return binarized_nifti
+    return binarized_image
