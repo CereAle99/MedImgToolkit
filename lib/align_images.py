3de27d50bc2caa12cdc5ba3d72e2be34b8ea291a
Ale <alessandro@ceresi.eu>
2024-02-21 01:01:52 +0100

    added function documentation

--- lib/align_images.py
+++ lib/align_images.py
@@ -3,89 +3,98 @@ import nibabel as nib
 from scipy.ndimage import zoom, shift
 
 
-def align_images(pet_nifti, ct_nifti):
+def align_images(input_image, reference_image):
     """
-
+    Align two nibabel (nib) images with different resolution and pixel-size but spatially registered. 
+    The function keep the pixel-size of the reference_image and change the resoluition into 
+    a common resolution between the two images in order to align the images' voxels. 
+    
     Args:
-        pet_nifti:
-        ct_nifti:
+        input_image: nib
+            NIfTI file of the image to be aligned
+        reference_image: nib
+            the NIfTI file of the image taken for reference
 
-    Returns:
+    Returns: 
+        aligned_input: nib
+            input_image resized and aligned with the reference_image
+        aligned_reference: nib
+            input_image resized and aligned with the reference_image
 
     """
 
     # Load the nifti files header and image array
-    pet_header = pet_nifti.header
-    ct_header = ct_nifti.header
-    pet_affine = pet_nifti.affine
-    ct_affine = ct_nifti.affine
-    pet_image = pet_nifti.get_fdata()
-    ct_image = ct_nifti.get_fdata()
+    img_header = input_image.header
+    ref_header = reference_image.header
+    img_affine = input_image.affine
+    ref_affine = reference_image.affine
+    img_array = input_image.get_fdata()
+    ref_array = reference_image.get_fdata()
 
     # PET resize ratio
-    resize_ratio = pet_header['pixdim'][1:4] / ct_header['pixdim'][1:4]
+    resize_ratio = img_header['pixdim'][1:4] / ref_header['pixdim'][1:4]
 
     # PET resizing and his displacement
-    pet_image = zoom(pet_image, zoom=resize_ratio, grid_mode=True, mode="grid-constant")
-    rest = np.array(pet_image.shape) - np.array(pet_header['dim'][1:4]) * np.array(resize_ratio)
-    pixel_displacement = rest / np.array(pet_image.shape)
+    img_array = zoom(img_array, zoom=resize_ratio, grid_mode=True, mode="grid-constant")
+    rest = np.array(img_array.shape) - np.array(img_header['dim'][1:4]) * np.array(resize_ratio)
+    pixel_displacement = rest / np.array(img_array.shape)
 
     # CT image resizing
-    ct_image_resized = np.zeros(shape=pet_image.shape)
-    side_x = (pet_image.shape[0] - ct_image.shape[0]) // 2
-    side_y = (pet_image.shape[1] - ct_image.shape[1]) // 2
-    side_z = (pet_image.shape[2] - ct_image.shape[2]) // 2
-    center_x = ct_image.shape[0]
-    center_y = ct_image.shape[1]
-    center_z = ct_image.shape[2]
-    ct_image_resized[side_x:side_x + center_x, side_y:side_y + center_y, side_z:side_z + center_z] = ct_image
+    ref_array_resized = np.zeros(shape=img_array.shape)
+    side_x = (img_array.shape[0] - ref_array.shape[0]) // 2
+    side_y = (img_array.shape[1] - ref_array.shape[1]) // 2
+    side_z = (img_array.shape[2] - ref_array.shape[2]) // 2
+    center_x = ref_array.shape[0]
+    center_y = ref_array.shape[1]
+    center_z = ref_array.shape[2]
+    ref_array_resized[side_x:side_x + center_x, side_y:side_y + center_y, side_z:side_z + center_z] = ref_array
 
     # Image axis orientations
-    x_orientation = np.sign(pet_header['srow_x'][0])
-    y_orientation = np.sign(pet_header['srow_y'][1])
-    z_orientation = np.sign(pet_header['srow_z'][2])
+    x_orientation = np.sign(img_header['srow_x'][0])
+    y_orientation = np.sign(img_header['srow_y'][1])
+    z_orientation = np.sign(img_header['srow_z'][2])
 
     # Managing the offset
-    pet_header['qoffset_x'] = (pet_header['qoffset_x']
-                               - x_orientation * (pet_header['pixdim'][1] / 2)
-                               + x_orientation * (pet_header['pixdim'][1] / resize_ratio[0]) / 2)
-    pet_header['qoffset_y'] = (pet_header['qoffset_y']
-                               - y_orientation * (pet_header['pixdim'][2] / 2)
-                               + y_orientation * (pet_header['pixdim'][2] / resize_ratio[1]) / 2)
-    pet_header['qoffset_z'] = (pet_header['qoffset_z']
-                               - z_orientation * (pet_header['pixdim'][3] / 2)
-                               + z_orientation * (pet_header['pixdim'][3] / resize_ratio[2]) / 2)
+    img_header['qoffset_x'] = (img_header['qoffset_x']
+                               - x_orientation * (img_header['pixdim'][1] / 2)
+                               + x_orientation * (img_header['pixdim'][1] / resize_ratio[0]) / 2)
+    img_header['qoffset_y'] = (img_header['qoffset_y']
+                               - y_orientation * (img_header['pixdim'][2] / 2)
+                               + y_orientation * (img_header['pixdim'][2] / resize_ratio[1]) / 2)
+    img_header['qoffset_z'] = (img_header['qoffset_z']
+                               - z_orientation * (img_header['pixdim'][3] / 2)
+                               + z_orientation * (img_header['pixdim'][3] / resize_ratio[2]) / 2)
 
     # PET header fixing
-    pet_header['dim'][1:4] = pet_image.shape
-    pet_affine[0, 0] = x_orientation * (pet_header['pixdim'][1] / resize_ratio[0] - pixel_displacement[0])
-    pet_affine[1, 1] = y_orientation * (pet_header['pixdim'][2] / resize_ratio[1] - pixel_displacement[1])
-    pet_affine[2, 2] = z_orientation * (pet_header['pixdim'][3] / resize_ratio[2] - pixel_displacement[2])
-    pet_affine[0, 3] = pet_header['qoffset_x']
-    pet_affine[1, 3] = pet_header['qoffset_y']
-    pet_affine[2, 3] = pet_header['qoffset_z']
+    img_header['dim'][1:4] = img_array.shape
+    img_affine[0, 0] = x_orientation * (img_header['pixdim'][1] / resize_ratio[0] - pixel_displacement[0])
+    img_affine[1, 1] = y_orientation * (img_header['pixdim'][2] / resize_ratio[1] - pixel_displacement[1])
+    img_affine[2, 2] = z_orientation * (img_header['pixdim'][3] / resize_ratio[2] - pixel_displacement[2])
+    img_affine[0, 3] = img_header['qoffset_x']
+    img_affine[1, 3] = img_header['qoffset_y']
+    img_affine[2, 3] = img_header['qoffset_z']
 
     # CT header fixing
-    ct_header['dim'][1:4] = pet_image.shape
-    ct_affine[0, 0] = x_orientation * (ct_header['pixdim'][1] - pixel_displacement[0])
-    ct_affine[1, 1] = y_orientation * (ct_header['pixdim'][2] - pixel_displacement[1])
-    ct_affine[2, 2] = z_orientation * (ct_header['pixdim'][3] - pixel_displacement[2])
-    ct_affine[0, 3] = ct_header['qoffset_x'] - side_x * (pet_affine[0, 0])
-    ct_affine[1, 3] = ct_header['qoffset_y'] - side_y * (pet_affine[1, 1])
-    ct_affine[2, 3] = ct_header['qoffset_z'] - side_z * (pet_affine[2, 2])
+    ref_header['dim'][1:4] = img_array.shape
+    ref_affine[0, 0] = x_orientation * (ref_header['pixdim'][1] - pixel_displacement[0])
+    ref_affine[1, 1] = y_orientation * (ref_header['pixdim'][2] - pixel_displacement[1])
+    ref_affine[2, 2] = z_orientation * (ref_header['pixdim'][3] - pixel_displacement[2])
+    ref_affine[0, 3] = ref_header['qoffset_x'] - side_x * (img_affine[0, 0])
+    ref_affine[1, 3] = ref_header['qoffset_y'] - side_y * (img_affine[1, 1])
+    ref_affine[2, 3] = ref_header['qoffset_z'] - side_z * (img_affine[2, 2])
 
     # Evaluate the offset and shift the PET image
     axis_directions = np.array([-x_orientation, -y_orientation, -z_orientation])
-    shift_vector = (ct_affine[0:3, 3] - pet_affine[0:3, 3]) / np.abs(np.diag(pet_affine)[0:3]) * axis_directions
+    shift_vector = (ref_affine[0:3, 3] - img_affine[0:3, 3]) / np.abs(np.diag(img_affine)[0:3]) * axis_directions
     print(shift_vector)
-    pet_image = shift(pet_image, shift_vector, mode="constant", cval=0)
+    img_array = shift(img_array, shift_vector, mode="constant", cval=0)
 
     # Fix the PET offset
-    pet_affine[0, 3] = ct_affine[0, 3]
-    pet_affine[1, 3] = ct_affine[1, 3]
-    pet_affine[2, 3] = ct_affine[2, 3]
+    img_affine[0, 3] = ref_affine[0, 3]
+    img_affine[1, 3] = ref_affine[1, 3]
+    img_affine[2, 3] = ref_affine[2, 3]
 
     # CT and PET NIfTI files assembled
-    resized_pet = nib.Nifti1Image(pet_image, pet_affine, pet_header)
-    resized_ct = nib.Nifti1Image(ct_image_resized, ct_affine, ct_header)
-    return resized_pet, resized_ct
+    aligned_input = nib.Nifti1Image(img_array, img_affine, img_header)
+    aligned_reference = nib.Nifti1Image(ref_array_resized, ref_affine, ref_header)
+    return aligned_input, aligned_reference
