7b7619e863cb99744799b4abbd11d97fa8e80187
Ale <alessandro@ceresi.eu>
2024-02-22 00:24:49 +0100

    added documentation

--- lib/dilate.py
+++ lib/dilate.py
@@ -1,28 +1,43 @@
 import nibabel as nib
 from scipy import ndimage
-from lib.fill_holes import fill_spinal_holes
+from lib.fill_holes import fill_holes
 
 
-def dilate_spine(input_nifti, iterations=3, fill=False):
+def dilate(imput_image, iterations=1, fill=False, dim=3, n_dilations=None):
     """
-
+    Takes a nibabel 2D/3D image representing a binary mask and dilates the mask 
+    a number of times given by the "iterations" parameter. If the parameter 
+    "fill" is True firstly the function fill_holes is executed on the mask.
     Args:
-        input_nifti:
-        iterations:
-        fill:
+        input_image: nib
+            imput object to be shaped
+        iterations: int
+            number of iterations for the dilation. Default is 1
+        fill: bool
+            if False, only the dilation is performed. If True before the 
+            dilation a binary filling operation is performed. Default is 
+            False
+        dim: int
+            dimensions of the squared/cubic structuring element in pixel for
+            the filling operation. Default is 3
+        n_dilations: int
+            number of iterations for the dilations and then erosions inside
+            the filling operation. Default is None
 
-    Returns:
+
+    Returns: nib
+        nibabel object representing the binary mask enclosed in a cilinder
 
     """
     if fill:
-        fill_nifti = fill_spinal_holes(input_nifti, 3)
+        fill_nifti = fill_holes(imput_image, dim=dim, n_dilations=n_dilations)
     else:
-        fill_nifti = input_nifti
+        fill_nifti = imput_image
 
     image = fill_nifti.get_fdata()
     final_image = ndimage.binary_dilation(image, iterations=iterations)
 
     # Put the image in a NIfTI file
-    final_nifti = nib.Nifti1Image(final_image, input_nifti.affine, input_nifti.header)
+    final_nifti = nib.Nifti1Image(final_image, imput_image.affine, imput_image.header)
 
     return final_nifti
