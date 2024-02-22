ac5e7e1219b79b240c39cd82d1dfd8ab08d88f68
Ale <alessandro@ceresi.eu>
2024-02-22 01:14:11 +0100

    added documentation

--- lib/crop.py
+++ lib/crop.py
@@ -1,46 +1,76 @@
 import nibabel as nib
-from lib.fill_holes import fill_spinal_holes
-from lib.dilate import dilate_spine
-from lib.cylinder import spine_as_cylinder
+from lib.fill_holes import fill_holes
+from lib.dilate import dilate
+from lib.cylinder import cylinder
 from lib.binarize import binarize
 
 
-def crop_spine_from_ct(input_nifti, mask, shape="original", segmentation_value=1, f_dilations=3, f_dim=3, d_dilations=3, d_filling=True, c_dilations=3):
+def crop(input_nifti, mask, shape="original", label=1, f_dim=3, f_dilations=3, d_iterations=3, d_filling=True, d_dilations=3, c_dilations=3):
     """
-
+    Takes an image and its segmentation, binarizes it for a specific label,
+    shapes the binary mask according to the "shape" parameter, and performs
+    the multiplication between the image and the mask to keep just the image
+    values inside the segmentation area, while all the others are set to zero.
+    
     Args:
-        input_nifti:
-        mask:
-        shape:
-        segmentation_value:
-        f_dilations:
-        f_dim:
-        d_dilations:
-        d_filling:
-        c_dilations:
+        input_nifti: nib
+            imput object to be shaped
+        mask: nib
+            segmentation object selecting the area to crop
+        shape: choices: {'original', 'fill_holes', 'dilation', 'cylinder'}
+            selection of the shape for the image to be cropped. Default is
+            'original'
+        label: float or int
+            label of the voxels to be part of the final mask. Default is 1
+        f_dim: int
+            dimensions of the squared/cubic structuring element in pixel.
+            It is used only if the selected shape is 'fill_holes'.
+            Default is 3.
+        f_dilations: int
+            number of iterations for the dilations before the filling 
+            operation, and of erosions after. It is used only if the selected
+            shape is 'fill_holes'. Default is 3
+        d_iterations: int
+            number of iterations for the dilation. It is used only if the
+            selected shape is 'dilation'. Default is 3
+        d_filling: bool
+            if False, only the dilation is performed. If True before the 
+            dilation a binary filling operation is performed. It is used 
+            only if the selected shape is 'dilation'. Default is True
+        d_dilations:: int
+            number of iterations for the dilations and then erosions inside
+            the filling operation. It is used only if the selected shape is
+            'dilation'. Default is 3
+        c_dilations: int
+            number of dilations to perform before the cylinder shaping to the
+            original mask. It is used only if the selected shape is 'dilation'.
+            Default is 3
 
     Returns:
-
+        crop_image: nib
+            input image shaped conserving just the information in the area 
+            defined by the binary mask
+        segm_aligned:
+            binary mask shaped
     """
 
-    # Binarize the mask
-    binarized_mask = binarize(mask, segmentation_value)
+    # mask binarization
+    mask = binarize(mask, label=label)
 
     # Apply shape function on segmentation
     if shape == "fill_holes":
         print(shape)
-        binarized_mask = fill_spinal_holes(binarized_mask, n_dilations=f_dilations, dim=f_dim)
-    elif shape == "dilation":
+        mask = fill_holes(mask, dim=f_dim, n_dilations=f_dilations)
+    elif shape == "dilate":
         print(shape)
-        binarized_mask = dilate_spine(binarized_mask, d_dilations, d_filling)
+        mask = dilate(mask, iterations=d_iterations, fill=d_filling, n_dilations=d_dilations)
     elif shape == "cylinder":
         print(shape)
-        binarized_mask = spine_as_cylinder(binarized_mask, c_dilations)
+        mask = cylinder(mask, c_dilations)
     elif shape == "original":
         print(shape)
     else:
         print("Shape invalid. Going with the original shape.")
-    print("done shaping")
 
     # Put the segmentation into a numpy array
     segmentation = binarized_mask.get_fdata()
