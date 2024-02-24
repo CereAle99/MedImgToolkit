09c474edffb7706faf2126c1efe3d59a7e04916d
Ale <alessandro@ceresi.eu>
2024-02-25 00:39:57 +0100

    add return niftiimage test

---
+++ tests/test_binarize.py
@@ -0,0 +1,26 @@
+import nibabel as nib
+from lib.binarize import binarize
+import numpy as np
+import os
+import pytest
+
+@pytest.fixture
+def sample_multilabel_segmentation(tmp_path):
+    sample_file_path = os.path.join('data', 'segmentation.nii')
+    return nib.load(sample_file_path)
+
+
+def test_binarize_returns_nifti1image(sample_multilabel_segmentation):
+    """
+    Tests:
+    If the output is a NiftiImage instance
+    If the input image has the same resolution of the output image
+    If the datatype of the output image is binary
+    """
+    result = binarize(sample_multilabel_segmentation)
+    result_array = result.get_fdata()
+    sample_array = sample_multilabel_segmentation.get.fdata()
+
+    assert isinstance(result, nib.Nifti1Image)
+    assert result_array.shape == sample_array.shape
+    assert result.get_data_dtype() == np.uint8
