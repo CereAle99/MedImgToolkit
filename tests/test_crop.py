78a81eb6b6c10a7d5926bca83baeccafdff0a124
Ale <alessandro@ceresi.eu>
2024-02-25 00:28:05 +0100

    add fixture to load images

---
+++ tests/test_crop.py
@@ -0,0 +1,15 @@
+import nibabel as nib
+from lib.allign_crop import align_images
+import os
+import pytest
+
+@pytest.fixture
+def sample_multilabel_segmentation(tmp_path):
+    sample_file_path = os.path.join('data', 'segmentation.nii')
+    return nib.load(sample_file_path)
+
+
+@pytest.fixture
+def sample_medical_image(tmp_path):
+    sample_file_path = os.path.join('data', 'CT.nii')
+    return nib.load(sample_file_path)
