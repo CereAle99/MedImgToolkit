ce025a5845a4ca8c723ddace3fed8d7050a0ec38
Ale <alessandro@ceresi.eu>
2024-03-09 14:14:06 +0100

    added created fixtures for testing if the dilation is working

--- tests/test_dilate.py
+++ tests/test_dilate.py
@@ -20,6 +20,61 @@ def sample_singlelabel_segmentation():
     return nib.load(sample_file_path)
 
 
+@pytest.fixture
+def sample_image_center_pixel():
+    """
+    Create a 5x5x5 cube with a center made of 1s with borders made of 0s
+    Put it in a NIfTI image
+    """
+    data = np.zeros((5, 5, 5), dtype=np.uint8)
+    data[2, 2, 2] = 1 
+
+    nifti_image = nib.Nifti1Image(data, affine=np.eye(4))
+
+    return nifti_image
+
+
+@pytest.fixture
+def sample_image_center_big_cross():
+    """
+    Create a 9x9x9 cube with a center made of 1s with borders made of 0s
+    Put it in a NIfTI image
+    """
+    data = np.zeros((5, 5, 5), dtype=int)
+    data[1:4, 1:4, 1:4] = 1
+    data[2, 2, 0] = 1
+    data[0, 2, 2] = 1
+    data[2, 0, 2] = 1
+    data[2, 4, 2] = 1
+    data[4, 2, 2] = 1
+    data[2, 2, 4] = 1
+
+    nifti_image = nib.Nifti1Image(data, affine=np.eye(4))
+
+    return nifti_image
+
+
+
+@pytest.fixture
+def sample_image_center_cross():
+    """
+    Create a 9x9x9 cube with a center made of 1s with borders made of 0s
+    Put it in a NIfTI image
+    """
+    data = np.zeros((5, 5, 5), dtype=int)
+    data[2, 2, 2] = 1
+    data[2, 2, 1] = 1
+    data[2, 2, 3] = 1
+    data[2, 1, 2] = 1
+    data[2, 3, 2] = 1
+    data[1, 2, 2] = 1
+    data[3, 2, 2] = 1
+
+    nifti_image = nib.Nifti1Image(data, affine=np.eye(4))
+
+    return nifti_image
+
+
 def test_dilate_returns_nifti1image(sample_singlelabel_segmentation):
     """
     Tests:
@@ -28,11 +83,11 @@ def test_dilate_returns_nifti1image(sample_singlelabel_segmentation):
     If the datatype of the output image is binary
     """
     result = dilate(sample_singlelabel_segmentation)
-    result_array = result.get_fdata()
-    sample_array = sample_singlelabel_segmentation.get_fdata()
+    result_data = result.get_fdata()
+    sample_data = sample_singlelabel_segmentation.get_fdata()
 
     assert isinstance(result, nib.Nifti1Image)
-    assert result_array.shape == sample_array.shape
+    assert result_data.shape == sample_data.shape
     assert result.get_data_dtype() == np.uint8
 
 
@@ -68,7 +123,7 @@ def test_dilate_output_differs_from_input(sample_singlelabel_segmentation):
     input_image = sample_singlelabel_segmentation.get_fdata()
     output_image = dilate(sample_singlelabel_segmentation)
     
-    assert not np.array_equal(input_image, output_image)
+    assert not np.data_equal(input_image, output_image)
 
 
 def test_dilate_iterations_param_limits(sample_singlelabel_segmentation):
@@ -103,3 +158,35 @@ def test_dilate_segmentation_is_bigger(sample_singlelabel_segmentation):
     input_mask = sample_singlelabel_segmentation.get_fdata()
 
     assert np.sum(dilate_mask, axis=(0,1,2)) > np.sum(input_mask, axis=(0,1,2))
+
+
+def test_dilate_dilating_pixel(sample_image_center_pixel, sample_image_center_cross):
+    """
+    The dilate function get as input a 5x5x5 nibabel image with just a 
+    certral pixel.
+
+    Tests:
+    If the output of the dilation is an identical image with a 3x3x3
+    centered cross
+    """
+
+    dilate_mask = dilate(sample_image_center_pixel).get_fdata()
+    cross_mask = sample_image_center_cross.get_fdata()
+
+    assert np.all(dilate_mask == cross_mask)
+
+
+def test_dilate_dilating_pixel(sample_image_center_pixel, sample_image_center_cross):
+    """
+    The dilate function get as input a 5x5x5 nibabel image with just a 
+    certral pixel.
+
+    Tests:
+    If the output of the dilation is an identical image with a 5x5x5
+    centered cross with a 3x3x3 center cube
+    """
+
+    dilate_mask = dilate(sample_image_center_pixel, iterations=2).get_fdata()
+    cross_mask = sample_image_center_cross.get_fdata()
+
+    assert np.all(dilate_mask == cross_mask)
