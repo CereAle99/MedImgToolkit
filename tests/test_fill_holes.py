ba5c9970b05c972fea5728ee39b108b7ba21d957
Ale <alessandro@ceresi.eu>
2024-03-11 19:24:05 +0100

    improved tests' documentation

--- tests/test_fill_holes.py
+++ tests/test_fill_holes.py
@@ -16,7 +16,7 @@ from lib.fill_holes import fill_holes
 @pytest.fixture
 def sample_singlelabel_segmentation():
     """
-    Load a nib segmentation with one single label number
+    Fixture: Load a NIfTI segmentation with one single label number.
     """
     sample_file_path = os.path.join('data', 'segmentation_singlelabel.nii.gz')
     return nib.load(sample_file_path)
@@ -24,8 +24,7 @@ def sample_singlelabel_segmentation():
 @pytest.fixture
 def sample_image_with_hole():
     """
-    Create a 5x5x5 cube with a hole in the center and borders made of 0s
-    Put it in a NIfTI image
+    Fixture: Load a NIfTI 5x5x5 cube with a hole in the center and borders made of 0s
     """
     data = np.zeros((5, 5, 5), dtype=np.uint8)
     data[1:4, 1:4, 1:4] = 1 
@@ -38,8 +37,7 @@ def sample_image_with_hole():
 @pytest.fixture
 def sample_image_without_hole():
     """
-    Create a 5x5x5 cube with a center made of 1s with borders made of 0s
-    Put it in a NIfTI image
+    Fixture: Load a NIfTI 5x5x5 cube with a center made of 1s with borders made of 0s
     """
     data = np.zeros((5, 5, 5), dtype=np.uint8)
     data[1:4, 1:4, 1:4] = 1 
@@ -52,8 +50,7 @@ def sample_image_without_hole():
 @pytest.fixture
 def sample_image_with_hole_9():
     """
-    Create a 9x9x9 cube with a hole in the center and borders made of 0s
-    Put it in a NIfTI image
+    Fixture: Load a NIfTI 9x9x9 cube with a 5x5x5 hole in the center and borders made of 0s
     """
     data = np.zeros((9, 9, 9), dtype=np.uint8)
     data[1:8, 1:8, 1:8] = 1 
@@ -66,8 +63,7 @@ def sample_image_with_hole_9():
 @pytest.fixture
 def sample_image_without_hole_9():
     """
-    Create a 9x9x9 cube with a center made of 1s with borders made of 0s
-    Put it in a NIfTI image
+    Fixture: Load a NIfTI 9x9x9 cube with a centered 7x7x7 cube of 1s with borders made of 0s
     """
     data = np.zeros((9, 9, 9), dtype=np.uint8)
     data[1:8, 1:8, 1:8] = 1 
@@ -82,10 +78,12 @@ def sample_image_without_hole_9():
 
 def test_fill_holes_returns_nifti1image(sample_singlelabel_segmentation):
     """
-    Tests:
-    If the output is a NiftiImage instance
-    If the input image has the same resolution of the output image
-    If the datatype of the output image is binary
+    Giving a single-label segmentation to the fill_holes function
+
+    tests:
+    - If the output is a NiftiImage instance
+    - If the input image has the same resolution of the output image
+    - If the datatype of the output image is made of np.unit8 numbers
     """
     result = fill_holes(sample_singlelabel_segmentation)
     result_array = result.get_fdata()
@@ -98,8 +96,10 @@ def test_fill_holes_returns_nifti1image(sample_singlelabel_segmentation):
 
 def test_fill_holes_empty_input():
     """
-    Tests:
-    If the function gets an empty input
+    Giving an empty segmentation to the fill_holes function
+    tests:
+    - If the function raises a ValueError with the message
+    "Input file is empty."
     """
 
     empty_image = nib.Nifti1Image(np.zeros((100, 100, 100)), np.eye(4))
@@ -110,8 +110,11 @@ def test_fill_holes_empty_input():
 
 def test_fill_holes_encloses_mask(sample_singlelabel_segmentation):
     """
-    Tests:
-    If the output encloses the input image
+    Giving a single-label segmentation to the fill_holes function
+
+    tests:
+    - If the output image encloses the input image, that means that all the 
+    voxel having 1s in the input image are 1s also in the output image
     """
 
     fill_holes_mask = fill_holes(sample_singlelabel_segmentation).get_fdata()
@@ -122,8 +125,10 @@ def test_fill_holes_encloses_mask(sample_singlelabel_segmentation):
 
 def test_fill_holes_output_differs_from_input(sample_singlelabel_segmentation):
     """
-    Tests:
-    If input and output are different
+    Giving a single-label segmentation to the fill_holes function
+
+    tests:
+    - If input and output are different images
     """
     input_image = sample_singlelabel_segmentation.get_fdata()
     output_image = fill_holes(sample_singlelabel_segmentation)
@@ -133,8 +138,10 @@ def test_fill_holes_output_differs_from_input(sample_singlelabel_segmentation):
 
 def test_fill_holes_dim_param_limits(sample_singlelabel_segmentation):
     """
-    Tests:
-    If giving 0 as structuring element raises an error
+    Giving a segmentation to the fill_holes function
+
+    tests:
+    - If Giving 0 as structuring element raises an error
     """
 
     with pytest.raises(ValueError, match="Dim 0 for the structuring element"):
@@ -143,12 +150,16 @@ def test_fill_holes_dim_param_limits(sample_singlelabel_segmentation):
 
 def test_fill_holes_dim_param_hole_mgnitude(sample_singlelabel_segmentation):
     """
-    Tests:
-    If with a higher dim parameter the holes results equal or smaller 
+    Giving a single-label segmentation to the fill_holes function, and calling 
+    teh function once with a dim parameter of 1 and once with a dim parameter of 3
+
+    tests:
+    - If for the output of the function with the dim parameter of 3 the number of
+    0 labels is higher than for the output with a dim parameter of 1 
     """
 
     fill_holes_dim_1 = fill_holes(sample_singlelabel_segmentation, dim=1).get_fdata()
-    fill_hole_dim_3 = fill_holes(sample_singlelabel_segmentation, dim=3).get_fdata()
+    fill_hole_dim_3 = fill_holes(sample_singlelabel_segmentation, dim=2).get_fdata()
 
     contiguous_holes, _ = sp.ndimage.label(fill_holes_dim_1)
     contiguous_holes_dim3, _ = sp.ndimage.label(fill_hole_dim_3)
@@ -159,11 +170,11 @@ def test_fill_holes_dim_param_hole_mgnitude(sample_singlelabel_segmentation):
 
 def test_fill_holes_filling_hole(sample_image_with_hole, sample_image_without_hole):
     """
-    Given as input a nibabel image with a a 1x1x1 hole in the center,
-    performs the fill_holes functionon.
+    Giving as input a segmentation with a a 3x3x3 centered cube made of 1s
+    with a hole in the center (label 0), performs the fill_holes functionon.
 
-    Tests:
-    If the output is an identical image with the hole filled
+    tests:
+    - If the output represents an image with the center hole filled
     """ 
 
     fill_holes_mask = fill_holes(sample_image_with_hole).get_fdata()
@@ -173,12 +184,12 @@ def test_fill_holes_filling_hole(sample_image_with_hole, sample_image_without_ho
 
 def test_fill_holes_filling_greater_hole(sample_image_with_hole_9, sample_image_without_hole_9):
     """
-    Given as input a nibabel image with a 5x5x5 hole in the center,
-    performs the fill_holes functionon.
+    Giving as input a segmentation with a a 3x3x3 centered cube made of 1s
+    with a hole in the center (label 0), performs the fill_holes functionon
 
-    Tests:
-    If the output is an identical image with the hole filled
-    """
+    tests:
+    - If the output represents an image with the center hole filled
+    """ 
 
     fill_holes_mask = fill_holes(sample_image_with_hole_9).get_fdata()
 
