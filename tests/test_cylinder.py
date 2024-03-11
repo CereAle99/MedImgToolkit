0dc885cd5b8ac3025756ddfb27921c7658f169d7
Ale <alessandro@ceresi.eu>
2024-03-11 20:38:54 +0100

    improved documentation and added test

--- tests/test_cylinder.py
+++ tests/test_cylinder.py
@@ -17,16 +17,75 @@ from lib.cylinder import cylinder
 
 @pytest.fixture
 def sample_singlelabel_segmentation():
+    """
+    Fixture: Load a NIfTI segmentation with one single label number.
+    """
     sample_file_path = os.path.join('data', 'segmentation_singlelabel.nii.gz')
     return nib.load(sample_file_path)
 
 
+@pytest.fixture
+def sample_image_center_pixel():
+    """
+    Fixture: load a NIfTI 5x5x5 cubic array with 1 in the center and the 
+    other voxels made of 0s
+    """
+    data = np.zeros((5, 5, 5), dtype=np.uint8)
+    data[2, 2, 2] = 1 
+
+    nifti_image = nib.Nifti1Image(data, affine=np.eye(4))
+
+    return nifti_image
+
+
+@pytest.fixture
+def sample_image_center_cross():
+    """
+    Fixture: load a NIfTI 5x5x5 image with a 3x3x3 cubic center made of 1s 
+    with borders made of 0s
+
+    """
+    data = np.zeros((5, 5, 5), dtype=int)
+    data[2, 2, 2] = 1
+    data[2, 2, 1] = 1
+    data[2, 2, 3] = 1
+    data[2, 1, 2] = 1
+    data[2, 3, 2] = 1
+    data[1, 2, 2] = 1
+    data[3, 2, 2] = 1
+
+    nifti_image = nib.Nifti1Image(data, affine=np.eye(4))
+
+    return nifti_image
+
+
+@pytest.fixture
+def sample_image_center_cylinder():
+    """
+    Fixture: load a NIfTI 5x5x5 image representing a 3x3x3 cylinder
+
+    """
+    data = np.zeros((5, 5, 5), dtype=int)
+    data[2, 1, 1:3] = 1
+    data[1, 2, 1:3] = 1
+    data[2, 3, 1:3] = 1
+    data[3, 2, 1:3] = 1
+
+
+    nifti_image = nib.Nifti1Image(data, affine=np.eye(4))
+
+    return nifti_image
+
+
+
 def test_cylinder_returns_nifti1image(sample_singlelabel_segmentation):
     """
-    Tests:
-    If the output is a NiftiImage instance
-    If the input image has the same resolution of the output image
-    If the datatype of the output image is binary
+    Giving to the cylinder function a NiftiImage instance
+
+    tests:
+    - If the output is a NiftiImage instance
+    - If the input image has the same resolution of the output image
+    - If the datatype of the output image is made of np.unit8 numbers
     """
     result = cylinder(sample_singlelabel_segmentation)
     result_array = result.get_fdata()
@@ -39,8 +98,11 @@ def test_cylinder_returns_nifti1image(sample_singlelabel_segmentation):
 
 def test_cylinder_empty_input():
     """
-    Tests:
-    If the function gets an empty input
+    Giving an empty segmentation to the cylinder function
+
+    tests:
+    - If the function raises a ValueError with the message
+    "Input file is empty."
     """
 
     empty_image = nib.Nifti1Image(np.zeros((100, 100, 100)), np.eye(4))
@@ -51,8 +113,10 @@ def test_cylinder_empty_input():
 
 def test_cylinder_output_differs_from_input(sample_singlelabel_segmentation):
     """
-    Tests:
-    If input and output are different
+    Giving a single-label segmentation to the cylinder function
+
+    tests:
+    - If input and output are different images
     """
     input_image = sample_singlelabel_segmentation.get_fdata()
     output_image = cylinder(sample_singlelabel_segmentation)
@@ -62,8 +126,11 @@ def test_cylinder_output_differs_from_input(sample_singlelabel_segmentation):
 
 def test_cylinder_encloses_mask(sample_singlelabel_segmentation):
     """
-    Tests:
-    If the output encloses the input image
+    Giving a single-label segmentation to the cylinder function
+
+    tests:
+    - If the output image encloses the input image, that means that all the 
+    voxel having 1s in the input image are 1s also in the output image
     """
 
     cylinder_mask = cylinder(sample_singlelabel_segmentation).get_fdata()
@@ -75,16 +142,36 @@ def test_cylinder_encloses_mask(sample_singlelabel_segmentation):
 
 def test_cylinder_dilation_bigger(sample_singlelabel_segmentation):
     """
-    Tests:
-    If the output with dilation is bigger than the input without dilation
+    Giving a single-label segmentation to the cylinder function, and calling 
+    the function once without dilations parameter and once with a dilations
+    parameter of 1
+
+    tests:
+    - If the output with dilation has a number of 1 labels higher than the 
+    number of 1s present in the other output 
     """
 
     cylinder_mask = cylinder(sample_singlelabel_segmentation).get_fdata()
-    cylinder_dilated_mask = cylinder(sample_singlelabel_segmentation, 1).get_fdata()
+    cylinder_dilated_mask = cylinder(sample_singlelabel_segmentation, dilations=1).get_fdata()
 
     count_ones_array1 = np.sum(cylinder_mask == 1)
     count_ones_array2 = np.sum(cylinder_dilated_mask == 1)
 
-    assert count_ones_array1 < count_ones_array2
+    assert count_ones_array1 <= count_ones_array2
 
+
+def test_cylinder_encloses_cross(sample_image_center_cross, sample_image_center_cylinder):
+    """
+    Giving a 5x5x5 single-label segmentation with a 1 in the central 
+    voxel and 0s in the others to the dilation function
+
+    tests:
+    - If the output of the dilation represents a 5x5x5 
+    asterisk shaped image
+    """
+
+    cylinder_sample = sample_image_center_cylinder.get_fdata()
+    cylinder_mask = cylinder(sample_image_center_cross).get_fdata()
+
+    assert np.all(cylinder_mask == cylinder_sample)
 
