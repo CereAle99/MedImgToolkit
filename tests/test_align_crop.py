dc9cc2643bda3ace9422d92b8729f90605e1bc81
Ale <alessandro@ceresi.eu>
2024-03-12 09:21:17 +0100

    improved documentation

--- tests/test_align_crop.py
+++ tests/test_align_crop.py
@@ -37,8 +37,8 @@ def sample_medical_image():
 @pytest.fixture
 def sample_image_center_50_others():
     """
-    Fixture: load a NIfTI 5x5x5 cubic array with 1 in the center and the 
-    other voxels made of 0s
+    Fixture: load a NIfTI 5x5x5 cubic array with 50 in the center and the 
+    other voxels made random numbers uniformly distributed between 0 and 10
     """
     data = np.random.randint(0, 10, size=(5, 5, 5), dtype=int)
     data[2, 2, 2] = 50
@@ -51,7 +51,7 @@ def sample_image_center_50_others():
 @pytest.fixture
 def sample_image_center_50():
     """
-    Fixture: load a NIfTI 5x5x5 cubic array with 1 in the center and the 
+    Fixture: load a NIfTI 5x5x5 cubic array with 50 in the voxel (4,4,4) and the 
     other voxels made of 0s
     """
     data = np.zeros((5, 5, 5), dtype=np.uint8)
@@ -78,7 +78,8 @@ def sample_image_center_pixel():
 def sample_image_center_pixel_double():
     """
     Fixture: load a NIfTI 10x10x10 cubic array with 1 in the 2x2x2 centered
-    cube and the other voxels made of 0s
+    cube and the other voxels made of 0s. It has also an offset of 
+    (-0.25, -0.25, -0.25)
     """
     data = np.zeros((10, 10, 10), dtype=np.uint8)
     data[4:6, 4:6, 4:6] = 1 
@@ -94,8 +95,9 @@ def sample_image_center_pixel_double():
 @pytest.fixture
 def sample_image_center_pixel_double_50():
     """
-    Fixture: load a NIfTI 10x10x10 cubic array with 1 in the 2x2x2 centered
-    cube and the other voxels made of 0s
+    Fixture: load a NIfTI 10x10x10 cubic array with 50 in the 2x2x2 centered
+    cube and the other voxels made of 0s. It has also an offset of 
+    (-0.25, -0.25, -0.25)
     """
     data = np.zeros((10, 10, 10), dtype=np.uint8)
     data[4:6, 4:6, 4:6] = 50
@@ -161,17 +163,19 @@ def test_align_crop_with_shift(sample_image_center_50_others,
                              sample_image_center_50
                              ):
     """
-    Gets as input an image and a multilabel segmentation, and the invalid 
-    shape "wrong" is passed.
+    Giving a NIfTI 5x5x5 cubic array with 50 in the center and the 
+    other voxels made random numbers uniformly distributed between 0 and 10
+    and a NIfTI 5x5x5 cubic array with an uncentered voxel with of 1
+    and the other voxels made of 0s with offset set to the point 
+    (-1,-1,-1) to the alignment_crop function
 
     tests:
-    - If the function raises a ValueError with the message
-    "Shape invalid."
+    - If the output image is an image with a 2x2x2 cube of 50 uncentered 
+    in th esame position of the second functiona passed
     """
 
-    image, segm = alignment_crop(sample_image_center_50_others, sample_image_segm_to_align, 'original', label=1)
+    image, _ = alignment_crop(sample_image_center_50_others, sample_image_segm_to_align, 'original', label=1)
     image = image.get_fdata()
-    segm = segm.get_fdata()
    
     assert np.all(np.round(image) == sample_image_center_50.get_fdata())
 
@@ -181,12 +185,15 @@ def test_align_crop_with_zoom(sample_image_center_50_others,
                              sample_image_center_pixel_double_50
                              ):
     """
-    Gets as input an image and a multilabel segmentation, and the invalid 
-    shape "wrong" is passed.
+    Giving a NIfTI 5x5x5 cubic array with 50 in the center and the 
+    other voxels made random numbers uniformly distributed between 0 and 10
+    and a NIfTI 10x10x10 cubic array with 1 in the 2x2x2 centered
+    cube and the other voxels made of 0s with an offset of 
+    (-0.25, -0.25, -0.25) to the alignment_crop function
 
     tests:
-    - If the function raises a ValueError with the message
-    "Shape invalid."
+    - If the output image is a NIfTI 10x10x10 cubic array with 50 in the 
+    2x2x2 centered cube and the other voxels made of 0s.
     """
 
     image, segm = alignment_crop(sample_image_center_50_others, sample_image_center_pixel_double, 'original', label=1)
